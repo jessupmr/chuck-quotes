2dd75e580b6d0550684b32d5cdfdb1a30905b16c
Mike Jessup <jessupmr@gmail.com>
2020-04-19 16:22:42 -0700

    Initial commit. Closes #1

---
+++ src/main/java/us/jessup/chuckquotes/ChuckQuotesApplication.java
@@ -0,0 +1,13 @@
+package us.jessup.chuckquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckQuotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckQuotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/us/jessup/chuckquotes/ChuckQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package us.jessup.chuckquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckQuotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
